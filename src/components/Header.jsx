6a86cf3b217c3673a3bc18bc21e6031c6356e3c1
openglobalmain <112708137+openglobalmain@users.noreply.github.com>
2022-12-17 20:34:59 +0700

    Начало пути

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://upload.wikimedia.org/wikipedia/ru/d/d3/Tomsk_beer_logo.png?20140525192340'></img>
+    </header >
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className='content'>
+    <div>
+      <img src='https://wallpaperaccess.com/full/346725.jpg' ></img>
+    </div>
+    <div>
+      Ava + description
+      <img src='https://i.insider.com/5484d9d1eab8ea3017b17e29?width=1000&format=jpeg&auto=webp' id='ava' ></img>
+    </div>
+    <div>
+      My posts
+      <div>
+        post 1
+      </div>
+      <div>
+        post 2
+      </div>
+      <div>
+        Aliqua eiusmod ullamco quis aliqua Lorem adipisicing aliquip irure incididunt do aute sit. Labore irure voluptate excepteur ipsum ipsum voluptate anim. In proident non labore officia qui amet. Minim non fugiat ex ut aliqua cupidatat consequat id labore fugiat. Voluptate mollit sit ipsum quis Lorem quis enim Lorem elit nulla dolore consequat elit elit. Incididunt mollit non amet officia non tempor culpa velit deserunt aliquip qui ullamco excepteur aliquip.
+      </div>
+    </div>
+  </div>
+}
+export default Profile;
